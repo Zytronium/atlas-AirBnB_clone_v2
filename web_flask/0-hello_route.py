4ac1caf27c718bb57f63ed6a4a01c789917bc9ec
Zytronium <dastelljes@outlook.com>
2024-11-02 15:47:18 -0500

    PEP8 formatting

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def index():
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
