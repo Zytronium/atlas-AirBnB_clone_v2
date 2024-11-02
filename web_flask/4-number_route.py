622a63c289a65ae78af63277a1bea40cd54fbbb6
Zytronium <dastelljes@outlook.com>
2024-11-02 16:22:55 -0500

    did task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+flask thing for web server
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def n_is_numb(n):
+    if n.isdigit():
+        return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
