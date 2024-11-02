e2299c4e916dcd33660fb646b335d130b029bdd7
Zytronium <dastelljes@outlook.com>
2024-11-02 16:18:44 -0500

    formatting

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c_text(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text = 'is cool'):
+def python_text(text='is cool'):
     return f"Python {text.replace('_', ' ')}"
 
 
